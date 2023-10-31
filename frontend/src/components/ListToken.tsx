5bf1906a0d00814cab31480ee2bd5a1b9a2d0e68
devEMEL <dominichenry882@gmail.com>
2023-10-31 18:43:22 +0100

    pull

--- frontend/src/components/ListToken.tsx
+++ frontend/src/components/ListToken.tsx
@@ -20,7 +20,7 @@ const PER_BOX_MBR = 0.0025e6
 const PER_BYTE_MBR = 0.0004e6
 
 const ListToken = () => {
-  const [appId, setAppId] = useState<number>(464983859)
+  const [appId, setAppId] = useState<number>(0)
   const [assetId, setAssetId] = useState<bigint>(0n)
   const [startTimestamp, setStartTimestamp] = useState<bigint>(0n)
   const [endTimestamp, setEndTimestamp] = useState<bigint>(0n)
